c5bb073f6a6d6fb2e7045431e7c13f2539916a08
Eric Greene <eric@t4d.io>
2021-03-29 13:35:31 -0400

    add exercise 2

---
+++ ToolsAppDemo/ToolsAppDemo/Client/ViewModels/ColorForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToolsAppDemo.Client.ViewModels
+{
+  public class ColorForm
+  {
+    public string Name { get; set; }
+
+    public string HexCode { get; set; }
+  }
+}
---
+++ exercises/exercise-2.md
@@ -0,0 +1,5 @@
+# Exercise 2
+
+1. Add a form to the Car Tool to capture new car data.
+
+2. When the "Add Car" button is clicked, create a new Car model, generate an id, and add the car to the table.

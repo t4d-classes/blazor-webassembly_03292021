8798ffbc9e653abfaa8696cd58b61ba644bdd737
Eric Greene <eric@t4d.io>
2021-03-29 18:11:18 -0400

    add solution to exercise 4 not bonus

---
+++ ToolsAppDemo/ToolsAppDemo/Client/ViewModels/CarForm.cs
@@ -0,0 +1,11 @@
+namespace ToolsAppDemo.Client.ViewModels
+{
+  public class CarForm
+  {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public decimal Price { get; set; }
+  }
+}
